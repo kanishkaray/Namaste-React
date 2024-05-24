550ccd7070ce9cc917792c09fcd0c883ce835812
Kanishkaray <kanishkaray12@gmail.com>
2024-05-24 17:33:12 +0530

    basics1

---
+++ 1. Basics1/App.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", {}, "Namaste EveryOne !!!");
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
